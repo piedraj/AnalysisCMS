e2b640491e5cd9e54614abc510690ff98aea4584
piedraj <jonatan.piedra@gmail.com>
2015-10-14 14:12:05 +0200

    Synchronized create and submit jobs

--- create-jobs.sh
+++ create-jobs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+shopt -s expand_aliases
+alias grepv='grep -v -e Run2015C -e QCD -e GluGluH -e HToWW -e WJetsToLNu_HT -e WZJets -e ttH'
+
+
 if [ $# -lt 2 ]; then
     echo "  "
     echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/50ns             50ns"
@@ -8,15 +13,13 @@ if [ $# -lt 2 ]; then
     echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/25ns             25ns"
     echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/25ns_August_PU 25ns"
     echo "  "
+    echo "  WARNING, will not read any latino tree that contains one of these strings"
+    echo "  "`alias | grep grepv | awk -F= '{print $2}' | sed "s/'//g" | sed "s/-e //g" | sed "s/grep -v //g"`
+    echo "  "
     exit -1
 fi
 
 export FILEPATH=$1
 export ERA=$2
 
-# Read all MC latino trees
-#ls -1 $FILEPATH | awk '{ print "./runWZ '$FILEPATH'/"$1,"'$ERA'" }'
-
-# Do not read some MC latino trees, like QCD
-ls -1 $FILEPATH | grep -v -e Run2015C -e QCD -e GluGluH -e HToWW -e WJetsToLNu_HT -e WZJets -e ttH | awk '{ print "./runWZ '$FILEPATH'/"$1,"'$ERA'" }'
-
+ls -1 $FILEPATH | grepv | awk '{ print "./runWZ '$FILEPATH'/"$1,"'$ERA'" }'
--- submit-jobs.sh
+++ submit-jobs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+shopt -s expand_aliases
+alias grepv='grep -v -e Run2015C -e QCD -e GluGluH -e HToWW -e WJetsToLNu_HT -e WZJets -e ttH'
+
+
 if [ $# -lt 2 ]; then
     echo "  "
     echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/50ns             50ns"
@@ -8,11 +13,14 @@ if [ $# -lt 2 ]; then
     echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/25ns             25ns"
     echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/25ns_August_PU 25ns"
     echo "  "
+    echo "  WARNING, will not read any latino tree that contains one of these strings"
+    echo "  "`alias | grep grepv | awk -F= '{print $2}' | sed "s/'//g" | sed "s/-e //g" | sed "s/grep -v //g"`
+    echo "  "
     exit -1
 fi
 
 export FILEPATH=$1
 export ERA=$2
-export NJOBS=`ls -1 $FILEPATH | grep -v QCD | wc -l`
+export NJOBS=`ls -1 $FILEPATH | grepv | wc -l`
 
 qsub -t 1-$NJOBS -v FILEPATH -v ERA settings.sge
