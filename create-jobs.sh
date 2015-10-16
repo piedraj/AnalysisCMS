ffd271aa1c75e1a30c3a033035749daa314e3d35
piedraj <jonatan.piedra@gmail.com>
2015-10-16 15:30:54 +0200

    Updated scripts

--- create-jobs.sh
+++ create-jobs.sh
@@ -7,14 +7,21 @@ alias grepv='grep -v -e Run2015C -e QCD -e GluGluH -e HToWW -e WJetsToLNu_HT -e 
 
 if [ $# -lt 2 ]; then
     echo "  "
-    echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/50ns             50ns"
-    echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/50ns_August_PU 50ns"
-    echo "  "
-    echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/25ns             25ns"
-    echo "  ./create-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/25ns_August_PU 25ns"
-    echo "  "
     echo "  WARNING, will not read any latino tree that contains one of these strings"
     echo "  "`alias | grep grepv | awk -F= '{print $2}' | sed "s/'//g" | sed "s/-e //g" | sed "s/grep -v //g"`
+    echo "  "
+    echo "  Please choose one of these two aliases"
+    echo "  "
+    echo "  alias latino2hist='./create-jobs.sh'"
+    echo "  alias latino2hist='./submit-jobs.sh'"
+    echo "  "
+    echo "  Please choose 50ns or 25ns data"
+    echo "  "
+    echo "  latino2hist /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/50ns             50ns"
+    echo "  latino2hist /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/50ns_August_PU 50ns"
+    echo "  "
+    echo "  latino2hist /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/25ns             25ns"
+    echo "  latino2hist /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/25ns_August_PU 25ns"
     echo "  "
     exit -1
 fi
--- submit-jobs.sh
+++ submit-jobs.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
 
-shopt -s expand_aliases
-alias grepv='grep -v -e Run2015C -e QCD -e GluGluH -e HToWW -e WJetsToLNu_HT -e WZJets -e ttH'
-
-
 if [ $# -lt 2 ]; then
-    echo "  "
-    echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/50ns             50ns"
-    echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/50ns_August_PU 50ns"
-    echo "  "
-    echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/Data13TeV/25ns             25ns"
-    echo "  ./submit-jobs.sh /gpfs/csic_projects/tier3data/LatinosSkims/MC_Spring15/25ns_August_PU 25ns"
-    echo "  "
-    echo "  WARNING, will not read any latino tree that contains one of these strings"
-    echo "  "`alias | grep grepv | awk -F= '{print $2}' | sed "s/'//g" | sed "s/-e //g" | sed "s/grep -v //g"`
-    echo "  "
+    ./create-jobs.sh
     exit -1
 fi
 
 
 echo "  "
-echo "  Compiling runWZ..."
+echo "  Let's play it safe. Compiling runWZ..."
 `root-config --cxx --cflags` -o runWZ runWZ.C AnalysisBase.C `root-config --glibs`
 
 
@@ -29,7 +16,6 @@ echo "  "
 echo "  Submitting jobs..."
 export FILEPATH=$1
 export ERA=$2
-export NJOBS=`ls -1 $FILEPATH | grepv | wc -l`
-
+export NJOBS=`./create-jobs.sh $FILEPATH $ERA | wc -l`
 qsub -t 1-$NJOBS -v FILEPATH -v ERA settings.sge
 echo "  "
