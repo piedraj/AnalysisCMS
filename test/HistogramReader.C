e8f823e8ea4e5541c7b414c4113c915505affbdf
piedraj <jonatan.piedra@gmail.com>
2015-11-09 20:19:39 +0100

    Minor scale change

--- test/HistogramReader.C
+++ test/HistogramReader.C
@@ -240,7 +240,7 @@ void HistogramReader::Draw(TString hname,
   if (pad1->GetLogy())
     {
       theMin = 1e-1;
-      theMax = 3. * TMath::Power(10, TMath::Log10(theMax) + 2);
+      theMax = TMath::Power(10, TMath::Log10(theMax) + 3);
     }
   else
     {
