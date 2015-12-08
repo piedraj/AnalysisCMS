3341d11f9c19e16f440c3246d7dfda641a3aa423
piedraj <jonatan.piedra@gmail.com>
2015-12-08 11:11:43 +0100

    Fixed minor bug

--- test/runPlotter.C
+++ test/runPlotter.C
@@ -57,7 +57,7 @@ void runPlotter(TString level)
       plotter.AddProcess("02_WZ",    "WZ",      kOrange-2);
       plotter.AddProcess("03_ZZ",    "ZZ",      kRed+3);
       plotter.AddProcess("04_Top",   "top",     kYellow);
-      plotter.AddProcess("10_Wg",    "W#gamma", kBlue);
+      plotter.AddProcess("10_Vg",    "V#gamma", kBlue);
       plotter.AddProcess("07_WJets", "W+jets",  kGray+1);
       plotter.AddProcess("06_ZJets", "Z+jets",  kGreen+2);
       plotter.AddProcess("08_TTV",   "ttV",     kGreen-6);
