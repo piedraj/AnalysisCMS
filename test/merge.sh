30d7d5c306fedec695a2ae4360c30e3be44fd494
piedraj <jonatan.piedra@gmail.com>
2016-01-22 17:04:25 +0100

    Split Vgamma

--- test/merge.sh
+++ test/merge.sh
@@ -20,8 +20,9 @@ hadd -f -k 06_ZJets.root DYJetsToLL_M-10to50.root DYJetsToLL_M-50.root
 hadd -f -k 07_WJets.root WJetsToLNu.root
 hadd -f -k 08_TTV.root   TTWJetsToLNu.root TTZToLLNuNu_M-10.root
 hadd -f -k 09_HWW.root   GluGluHToWWTo2L2Nu_M125.root
-hadd -f -k 10_Vg.root    Wg.root Zg.root
-hadd -f -k 11_VVV.root   WWZ.root WZZ.root ZZZ.root
+hadd -f -k 10_Wg.root    Wg.root
+hadd -f -k 11_Zg.root    Zg.root
+hadd -f -k 12_VVV.root   WWZ.root WZZ.root ZZZ.root
 
 popd
 
