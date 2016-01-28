2182ece8769beae23fa6615f7cc072737c06e51a
piedraj <jonatan.piedra@gmail.com>
2016-01-28 12:54:26 +0100

    Merged WW changes

--- test/merge.sh
+++ test/merge.sh
@@ -22,9 +22,10 @@ hadd -f -k 05_WW.root       WWTo2L2Nu.root GluGluWWTo2L2Nu_MCFM.root
 hadd -f -k 06_ZJets.root    DYJetsToLL_M-10to50.root DYJetsToLL_M-50.root
 hadd -f -k 07_WJets.root    WJetsToLNu.root
 hadd -f -k 08_TTV.root      TTWJetsToLNu.root TTZToLLNuNu_M-10.root
-hadd -f -k 09_HWW.root      GluGluHToWWTo2L2Nu_M125.root VBFHToWWTo2L2Nu_M125.root ttHJetToNonbb_M125.root HZJ_HToWW_M125.root
+hadd -f -k 09_HWW.root      GluGluHToWWTo2L2Nu_M125.root VBFHToWWTo2L2Nu_M125.root ttHJetToNonbb_M125.root
 hadd -f -k 10_Wg.root       Wg.root
 hadd -f -k 11_Zg.root       Zg.root
 hadd -f -k 12_VVV.root      WWZ.root WZZ.root ZZZ.root
+hadd -f -k 13_HZ.root       HZJ_HToWW_M125.root
 
 popd
