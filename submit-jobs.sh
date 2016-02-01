ebcb0a870cb7c8c889804e0e749effa77f2097e7
piedraj <jonatan.piedra@gmail.com>
2016-02-01 17:49:22 +0100

    Some minor updates

--- submit-jobs.sh
+++ submit-jobs.sh
@@ -2,10 +2,16 @@
 
 if [ $# -lt 1 ]; then
     echo "  "
-    echo "  ./submit-jobs.sh samples/samples_mc.txt"
+    echo "  Common samples"
+    echo "-----------------"
     echo "  ./submit-jobs.sh samples/samples_data.txt"
-    echo "  ./submit-jobs.sh samples/samples_monoh.txt"
     echo "  ./submit-jobs.sh samples/samples_datadriven.txt"
+    echo "  ./submit-jobs.sh samples/samples_mc.txt"
+    echo "  "
+    echo "  Analysis specific samples"
+    echo "----------------------------"
+    echo "  ./submit-jobs.sh samples/samples_monoh.txt"
+    echo "  ./submit-jobs.sh samples/samples_ttdm.txt"
     echo "  "
     exit -1
 fi
