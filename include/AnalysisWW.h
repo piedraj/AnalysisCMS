c0b9e98dae404bfc28b8229f073e42f4b518d474
piedraj <jonatan.piedra@gmail.com>
2016-02-03 17:42:06 +0100

    Some AnalysisWW cleanup

--- include/AnalysisWW.h
+++ include/AnalysisWW.h
@@ -25,16 +25,14 @@ class AnalysisWW : public AnalysisCMS
   // Analysis histograms
   //----------------------------------------------------------------------------
   TH2D*                  h_metvar_m2l[nchannel][ncut][njetbin+1];
-  TH1D*                  h_dphilmet1[nchannel][ncut][njetbin+1];
-  TH1D*                  h_dphilmet2[nchannel][ncut][njetbin+1];
-  TH1D*                  h_fullpmet[nchannel][ncut][njetbin+1];
-  TH1D*                  h_trkpmet[nchannel][ncut][njetbin+1];
-  TH1D*                  h_jetpt1[nchannel][ncut][njetbin+1];
-  TH1D*                  h_metphi[nchannel][ncut][njetbin+1];
-  TH1D*                  h_lepphi1[nchannel][ncut][njetbin+1];
-  TH1D*                  h_lepphi2[nchannel][ncut][njetbin+1];
-  TH1D*                  h_pt1_pdfUp[nchannel][ncut][njetbin+1];
-  TH1D*                  h_pt1_pdfDown[nchannel][ncut][njetbin+1];
+  TH1D*                  h_dphilmet1 [nchannel][ncut][njetbin+1];
+  TH1D*                  h_dphilmet2 [nchannel][ncut][njetbin+1];
+  TH1D*                  h_fullpmet  [nchannel][ncut][njetbin+1];
+  TH1D*                  h_trkpmet   [nchannel][ncut][njetbin+1];
+  TH1D*                  h_jetpt1    [nchannel][ncut][njetbin+1];
+  TH1D*                  h_metphi    [nchannel][ncut][njetbin+1];
+  TH1D*                  h_lepphi1   [nchannel][ncut][njetbin+1];
+  TH1D*                  h_lepphi2   [nchannel][ncut][njetbin+1];
 };
 
 #endif
