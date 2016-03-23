c14b50fb2ff9762a1b43002770349fe9392a4fda
piedraj <jonatan.piedra@gmail.com>
2016-03-23 14:56:29 +0100

    Updated MVA.C file

---
+++ tmva/MVA.C
@@ -0,0 +1,373 @@
+#include <iostream>
+
+#include "TFile.h"
+#include "TMVA/Config.h"
+#include "TMVA/Factory.h"
+#include "TMVA/MethodCuts.h"
+#include "TMVA/Reader.h"
+#include "TMVA/Tools.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TTree.h"
+
+
+void MVATrain(TString signal);
+void MVARead (TString signal,
+	      TString sample);
+
+
+const TString analysis  = "TTDM";
+const TString inputdir  = "../minitrees/";
+const TString outputdir = "training_test_outputs/trainOPT/";
+
+
+void MVA(TString signal = "06_WW")
+{
+  gSystem->mkdir(outputdir, kTRUE);
+  gSystem->mkdir("application_outputs/OPT", kTRUE);
+
+  MVATrain(signal);
+
+  MVARead(signal, signal);
+  MVARead(signal, "08_WJets");
+  MVARead(signal, "09_TTV");
+  MVARead(signal, "01_Data");
+  MVARead(signal, "07_ZJets");
+  MVARead(signal, "00_Fakes");
+  MVARead(signal, "02_WZTo3LNu");
+  MVARead(signal, "05_ST");
+  MVARead(signal, "03_ZZ");
+  MVARead(signal, "04_TTTo2L2Nu");
+}
+
+
+//------------------------------------------------------------------------------
+// MVATrain
+//------------------------------------------------------------------------------
+void MVATrain(TString signal)
+{
+  TFile* outputfile = TFile::Open(outputdir + signal + ".root", "recreate");
+
+  (TMVA::gConfig().GetIONames()).fWeightFileDir = "training_test_outputs/weightsOPT";
+   
+
+  // Factory
+  //----------------------------------------------------------------------------
+  TMVA::Factory* factory = new TMVA::Factory(signal, outputfile,    
+					     "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification");
+
+
+  // Get the trees
+  //----------------------------------------------------------------------------
+  TFile *inputSFile  = TFile::Open(inputdir + analysis + "/" + signal + ".root", "read");
+  TFile *inputB1File = TFile::Open(inputdir + analysis + "/08_WJets.root",       "read");
+  TFile *inputB2File = TFile::Open(inputdir + analysis + "/09_TTV.root",         "read");
+  TFile *inputB3File = TFile::Open(inputdir + analysis + "/01_Data.root",        "read");
+  TFile *inputB4File = TFile::Open(inputdir + analysis + "/07_ZJets.root",       "read");
+  TFile *inputB5File = TFile::Open(inputdir + analysis + "/00_Fakes.root",       "read");
+  TFile *inputB6File = TFile::Open(inputdir + analysis + "/02_WZTo3LNu.root",    "read");
+  TFile *inputB7File = TFile::Open(inputdir + analysis + "/05_ST.root",          "read");
+  TFile *inputB8File = TFile::Open(inputdir + analysis + "/03_ZZ.root",          "read");
+  TFile *inputB9File = TFile::Open(inputdir + analysis + "/04_TTTo2L2Nu.root",   "read");
+
+  TTree *signalTree  = (TTree*)inputSFile ->Get("latino");
+  TTree *background1 = (TTree*)inputB1File->Get("latino");
+  TTree *background2 = (TTree*)inputB2File->Get("latino");
+  TTree *background3 = (TTree*)inputB3File->Get("latino");
+  TTree *background4 = (TTree*)inputB4File->Get("latino");
+  TTree *background5 = (TTree*)inputB5File->Get("latino");
+  TTree *background6 = (TTree*)inputB6File->Get("latino");
+  TTree *background7 = (TTree*)inputB7File->Get("latino");
+  TTree *background8 = (TTree*)inputB8File->Get("latino");
+  TTree *background9 = (TTree*)inputB9File->Get("latino");
+
+  Double_t weight = 1.0;
+
+  factory->AddSignalTree    (signalTree , weight);
+  factory->AddBackgroundTree(background1, weight);
+  factory->AddBackgroundTree(background2, weight);
+  factory->AddBackgroundTree(background3, weight);
+  factory->AddBackgroundTree(background4, weight);
+  factory->AddBackgroundTree(background5, weight);
+  factory->AddBackgroundTree(background6, weight);
+  factory->AddBackgroundTree(background7, weight);
+  factory->AddBackgroundTree(background8, weight);
+  factory->AddBackgroundTree(background9, weight);
+  
+  factory->SetWeightExpression("eventW");
+
+
+  // Add variables
+  //----------------------------------------------------------------------------
+  // Be careful with the order: it must be respected at the application step
+  // factory->AddVariable("<var1>+<var2>", "pretty title", "unit", 'F');
+
+//factory->AddVariable("channel",      "", "", 'I');
+//factory->AddVariable("metPfType1",   "", "", 'F');  // Empty range too large?
+//factory->AddVariable("mll",          "", "", 'F');  // Empty range too large?
+  factory->AddVariable("njet",         "", "", 'F');
+//factory->AddVariable("nbjet20loose", "", "", 'I');  // The reader does not like int variables
+  factory->AddVariable("lep1pt",       "", "", 'F');
+  factory->AddVariable("lep2pt",       "", "", 'F');
+  factory->AddVariable("jet1pt",       "", "", 'F');
+//factory->AddVariable("jet2pt",       "", "", 'F');
+  factory->AddVariable("dphill",       "", "", 'F');
+  factory->AddVariable("dphilep1jet1", "", "", 'F');
+//factory->AddVariable("dphilep1jet2", "", "", 'F');
+  factory->AddVariable("dphilmet1",    "", "", 'F');
+  factory->AddVariable("dphilep2jet1", "", "", 'F');
+//factory->AddVariable("dphilep2jet2", "", "", 'F');
+  factory->AddVariable("dphilmet2",    "", "", 'F');
+//factory->AddVariable("dphijj",       "", "", 'F');
+  factory->AddVariable("dphijet1met",  "", "", 'F');
+//factory->AddVariable("dphijet2met",  "", "", 'F');
+  factory->AddVariable("dphillmet",    "", "", 'F');
+
+
+  // Preselection cuts and preparation
+  //----------------------------------------------------------------------------
+  TCut mycut = "";
+
+//factory->PrepareTrainingAndTestTree(mycut, ":nTrain_Signal=0:nTest_Signal=0:nTrain_Background=2000:nTest_Background=2000:SplitMode=Alternate:!V");
+  factory->PrepareTrainingAndTestTree(mycut, ":nTrain_Signal=1500:nTest_Signal=1500:nTrain_Background=2000:nTest_Background=2000:SplitMode=Alternate:!V");  // Faster
+
+
+  // Book MVA
+  //----------------------------------------------------------------------------
+  factory->BookMethod(TMVA::Types::kMLP, "MLP",
+		      "H:!V:NeuronType=sigmoid:VarTransform=N:NCycles=600:HiddenLayers=25,10:TestRate=5:!UseRegulator");
+
+
+  // Train, test and evaluate MVA
+  //----------------------------------------------------------------------------
+  factory->TrainAllMethods();     // Train using the set of training events
+  factory->TestAllMethods();      // Evaluate using the set of test events
+  factory->EvaluateAllMethods();  // Evaluate and compare performance
+
+
+  // Save the output
+  //----------------------------------------------------------------------------
+  outputfile->Close();
+
+  delete factory;
+}
+
+
+//------------------------------------------------------------------------------
+// MVARead
+//------------------------------------------------------------------------------
+void MVARead(TString signal, TString sample)
+{
+  TMVA::Reader* reader = new TMVA::Reader("!Color:!Silent");   
+
+//int   channel;
+//float metPfType1;
+//float mll;
+  float njet;
+//float nbjet20loose;  // It was saved as int, but it has to be float for the Reader
+  float lep1pt;
+  float lep2pt;
+  float jet1pt;
+//float jet2pt;
+  float dphill;
+  float dphilep1jet1;
+//float dphilep1jet2;
+  float dphilmet1;
+  float dphilep2jet1;
+//float dphilep2jet2;
+  float dphilmet2;
+//float dphijj;
+  float dphijet1met;
+//float dphijet2met;
+  float dphillmet;
+
+//reader->AddVariable("channel",      &channel);
+//reader->AddVariable("metPfType1",   &metPfType1);
+//reader->AddVariable("mll",          &mll);
+  reader->AddVariable("njet",         &njet);
+//reader->AddVariable("nbjet20loose", &nbjet20loose);
+  reader->AddVariable("lep1pt",       &lep1pt);
+  reader->AddVariable("lep2pt",       &lep2pt);
+  reader->AddVariable("jet1pt",       &jet1pt);
+//reader->AddVariable("jet2pt",       &jet2pt);
+  reader->AddVariable("dphill",       &dphill);
+  reader->AddVariable("dphilep1jet1", &dphilep1jet1);
+//reader->AddVariable("dphilep1jet2", &dphilep1jet2);
+  reader->AddVariable("dphilmet1",    &dphilmet1);
+  reader->AddVariable("dphilep2jet1", &dphilep2jet1);
+//reader->AddVariable("dphilep2jet2", &dphilep2jet2);
+  reader->AddVariable("dphilmet2",    &dphilmet2);
+//reader->AddVariable("dphijj",       &dphijj);
+  reader->AddVariable("dphijet1met",  &dphijet1met);
+//reader->AddVariable("dphijet2met",  &dphijet2met);
+  reader->AddVariable("dphillmet",    &dphillmet);
+
+
+  // Book MVA methods
+  //----------------------------------------------------------------------------
+  reader->BookMVA("MLP", "training_test_outputs/weightsOPT/" + signal + "_MLP.weights.xml");
+
+
+  // Get MVA response
+  //----------------------------------------------------------------------------
+  TH1F* hCuts = new TH1F("hCuts", "hCuts", 100, -0.1, 1.1);
+
+  TFile* input = TFile::Open(inputdir + analysis + "/" + sample + ".root", "update");
+
+  TTree* theTree = (TTree*)input->Get("latino");
+
+
+
+// output branch 
+float MVAresponse; 
+TBranch *MVABranch = theTree -> Branch( "MVA" + signal, &MVAresponse, "MVAresponse/F" );
+
+//load "auxliar variables":                                                                          !?
+//float <my_aux_var>;                                                                                !?
+//float weight                                                                                       !?
+//theTree->SetBranchAddress( "TChannel",  &<my_aux_var> ); //     CORTES                             !?
+//theTree->SetBranchAddress( "Weight"  ,  &weight       ); //     PESADO funcion Fillhistogram       !?
+
+//float var01, var02, var03, var04, var05, var06, var07, var08, var09, var10, var11, var12, var13, var14, var15, var16, var17, var18, var19, var20;
+
+
+//theTree -> SetBranchAddress( "channel"  , &var01 );
+//theTree -> SetBranchAddress( "MET"      , &var02 );
+//theTree -> SetBranchAddress( "mll"      , &var03 );
+//theTree -> SetBranchAddress( "njet"     , &var04 );
+//theTree -> SetBranchAddress( "nbjet15"  , &var05 );
+//theTree -> SetBranchAddress( "lep1Pt"   , &var06 );
+//theTree -> SetBranchAddress( "lep2Pt"   , &var07 );
+//theTree -> SetBranchAddress( "jetpt1"   , &var08 );
+//theTree -> SetBranchAddress( "jetpt2"   , &var09 );
+//
+//theTree -> SetBranchAddress( "dphilep1lep2", &var10 );
+//theTree -> SetBranchAddress( "dphilep1jet1", &var11 );
+//theTree -> SetBranchAddress( "dphilep1jet2", &var12 );
+//theTree -> SetBranchAddress( "dphilep1MET" , &var13 );
+//theTree -> SetBranchAddress( "dphilep2jet1", &var14 );
+//theTree -> SetBranchAddress( "dphilep2jet2", &var15 );
+//theTree -> SetBranchAddress( "dphilep2MET" , &var16 );
+//theTree -> SetBranchAddress( "dphijet1jet2", &var17 );
+//theTree -> SetBranchAddress( "dphijet1MET" , &var18 );
+//theTree -> SetBranchAddress( "dphijet2MET" , &var19 );
+//theTree -> SetBranchAddress( "dphilep1lep2MET" , &var20 );
+//theTree -> SetBranchAddress( "dphijet1jet2MET" , &var21 );
+
+
+
+
+
+
+
+
+
+
+
+
+//theTree->SetBranchAddress("channel",      &channel);
+//theTree->SetBranchAddress("metPfType1",   &metPfType1);
+//theTree->SetBranchAddress("mll",          &mll);
+  theTree->SetBranchAddress("njet",         &njet);
+//theTree->SetBranchAddress("nbjet20loose", &nbjet20loose);
+  theTree->SetBranchAddress("lep1pt",       &lep1pt);
+  theTree->SetBranchAddress("lep2pt",       &lep2pt);
+  theTree->SetBranchAddress("jet1pt",       &jet1pt);
+//theTree->SetBranchAddress("jet2pt",       &jet2pt);
+  theTree->SetBranchAddress("dphill",       &dphill);
+  theTree->SetBranchAddress("dphilep1jet1", &dphilep1jet1);
+//theTree->SetBranchAddress("dphilep1jet2", &dphilep1jet2);
+  theTree->SetBranchAddress("dphilmet1",    &dphilmet1);
+  theTree->SetBranchAddress("dphilep2jet1", &dphilep2jet1);
+//theTree->SetBranchAddress("dphilep2jet2", &dphilep2jet2);
+  theTree->SetBranchAddress("dphilmet2",    &dphilmet2);
+//theTree->SetBranchAddress("dphijj",       &dphijj);
+  theTree->SetBranchAddress("dphijet1met",  &dphijet1met);
+//theTree->SetBranchAddress("dphijet2met",  &dphijet2met);
+  theTree->SetBranchAddress("dphillmet",    &dphillmet);
+
+  float eventW;
+
+  theTree->SetBranchAddress("eventW",    &eventW);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for ( Long64_t ievt = 0; ievt < theTree -> GetEntries(); ievt++ ) {
+
+	if ( ievt % 500000 == 0 ) cout << "--- ... Processing event: " << ievt << endl;
+
+	theTree -> GetEntry(ievt);
+
+//	channel   = var01;
+//	MET       = var02;
+//	mll       = var03;
+//	njet      = var04;
+//	nbjet15   = var05;
+//	lep1Pt    = var06;
+//	lep2Pt    = var07;
+//	jetpt1    = var08;
+//	jetpt2    = var09;
+//	dphilep1lep2 = var10;
+//	dphilep1jet1 = var11;
+//	dphilep1jet2 = var12;
+//	dphilep1MET  = var13;
+//	dphilep2jet1 = var14;
+//	dphilep2jet2 = var15;
+//	dphilep2MET  = var16;
+//	dphijet1jet2 = var17;
+//	dphijet1MET  = var18;
+//	dphijet2MET  = var19;
+//	dphilep1lep2MET = var20;
+	//dphijet1jet2MET = var21;
+
+
+			MVAresponse = reader -> EvaluateMVA("MLP");
+			hCuts -> Fill(MVAresponse, eventW);
+			MVABranch -> Fill();
+
+}    
+                                            
+
+
+/////////////////////////////////
+
+//to add the MVA-branch to the input file
+theTree -> Write("", TObject::kOverwrite);
+
+input -> Close();
+
+
+
+
+
+//new file for the MVA-histogram
+//TFile *target  =  TFile::Open ("application_outputs/CR/h_MLP_"+sample+".root","UPDATE" );
+TFile *target  =  TFile::Open ("application_outputs/OPT/h_Cuts_" + signal + "_" + sample + ".root","recreate" );   
+
+hCuts -> Write();
+
+target -> Close();
+
+
+
+
+
+delete reader;
+} 
+
+
+
+
