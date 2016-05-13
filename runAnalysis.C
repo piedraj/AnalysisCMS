d58520d63d6d26c5d17449a17f67cc26e42a6f10
piedraj <jonatan.piedra@gmail.com>
2016-05-13 15:36:24 +0200

    Minor changes

--- runAnalysis.C
+++ runAnalysis.C
@@ -20,7 +20,7 @@ void runAnalysis(TString filename,
   //  AnalysisMonoH analysis(latino, systematic); analysis.Loop("MonoH", filename, lumi_fb);
   //  AnalysisStop  analysis(latino, systematic); analysis.Loop("Stop",  filename, lumi_fb);
   //  AnalysisTop   analysis(latino, systematic); analysis.Loop("Top",   filename, lumi_fb);
-  AnalysisTTDM  analysis(latino, systematic); analysis.Loop("TTDM",  filename, lumi_fb_blind);
+  AnalysisTTDM  analysis(latino, systematic); analysis.Loop("TTDM",  filename, lumi_fb);  // lumi_fb_blind
   //  AnalysisWW    analysis(latino, systematic); analysis.Loop("WW",    filename, lumi_fb);
   //  AnalysisWZ    analysis(latino, systematic); analysis.Loop("WZ",    filename, lumi_fb);
 }
