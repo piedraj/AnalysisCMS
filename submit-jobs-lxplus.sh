1650a064192b3a23c236b2443abd7db17a4856c6
piedraj <jonatan.piedra@gmail.com>
2016-06-10 17:28:53 +0200

    Minor changes

--- submit-jobs-lxplus.sh
+++ submit-jobs-lxplus.sh
@@ -2,12 +2,13 @@
 
 if [ $# -lt 2 ]; then
     echo "  "
-    echo "  ./submit-jobs-lxplus.sh samples/samples_data_2015D_74X_eos.txt       nominal"
-    echo "  ./submit-jobs-lxplus.sh samples/samples_mc_74X_eos.txt       nominal"
+    echo "  ./submit-jobs-lxplus.sh samples/samples_data_2015D_74X_eos.txt nominal"
+    echo "  ./submit-jobs-lxplus.sh samples/samples_mc_74X_eos.txt         nominal"
     echo "  ./submit-jobs-lxplus.sh samples/samples_stop_74X_eos.txt       nominal"
     echo "  "
     exit -1
 fi
+
 
 export WORKDIRECTORY=$PWD
 export SAMPLES=$1
@@ -21,6 +22,7 @@ echo "  Let's play it safe. Compiling runAnalysis..."
 ./make
 echo "  "
 
+
 # Submit jobs to the queues
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 export NJOBS=`cat $SAMPLES | grep latino | grep -v "#" | wc -l`
@@ -28,6 +30,4 @@ echo "  And... submitting" $NJOBS "jobs"
 echo "  "
 mkdir -p jobs
 cd jobs/
-bsub -q 1nh -J "settings[1-$NJOBS]" -o $WORKDIRECTORY/jobs $WORKDIRECTORY/settings.lsf WORKDIRECTORY SAMPLES SYSTEMATIC 
- 
-
+bsub -q 1nh -J "settings[1-$NJOBS]" -o $WORKDIRECTORY/jobs $WORKDIRECTORY/settings.lsf WORKDIRECTORY SAMPLES SYSTEMATIC
