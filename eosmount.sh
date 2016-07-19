3148336a74feb64ce235251e2d13690fecf9d6fa
Barbara Chazin Quero <bchazinq@lxplus064.cern.ch>
2016-07-19 16:19:26 +0200

    eos user

---
+++ eosmount.sh
@@ -0,0 +1,8 @@
+
+cd /tmp/bchazinq
+
+/afs/cern.ch/project/eos/installation/0.3.84-aquamarine.user/bin/eos.select -b fuse mount eosuser
+
+/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select -b fuse mount eos
+
+cd -
