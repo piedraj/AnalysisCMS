169d1a8cc4ca8dcce689b27f77baa12cad4310f5
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-07-22 10:49:22 +0200

    Added script to get MC samples from samples.py

---
+++ samples/get-mc-samples.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "  "
+    echo "  ./get-mc-samples.sh samples.py eos/cms/store/group/phys_higgs/cmshww/amassiro/HWW6p3/07Jun2016_spring16_mAODv2_6p3fbm1 > samples_mc_l2tight.txt"
+    echo "  "
+    exit -1
+fi
+
+export SAMPLES=$1
+export DIRECTORY=$2
+
+cat $SAMPLES | grep latino | grep -v "#" | grep -v "Run201" | awk 'BEGIN { FS = ".root" } ; { print $1 }' | awk -v dir=$DIRECTORY 'BEGIN { FS = "latino_" } ; { print dir"/latino_"$2".root" }'
