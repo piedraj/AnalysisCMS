c49397c49fdb8793cabb85e176ed372ffe1b23df
Juan Ferrero <juan.garcia.ferrero@cern.ch>
2016-09-21 11:47:58 +0200

    removing some dangerous lines

--- submit-jobs-lxplus.sh
+++ submit-jobs-lxplus.sh
@@ -3,7 +3,6 @@
 if [ $# -lt 2 ]; then
     echo "  "
     echo "  rm -rf jobs"
-    echo "  rm -rf rootfiles"
     echo "  "
     echo "  ./submit-jobs-lxplus.sh samples/80x/samples_data_l2tight.txt        nominal"
     echo "  ./submit-jobs-lxplus.sh samples/80x/samples_data_l2loose_fakeW.txt  nominal"
