b2a4bd4af482d25f527e62b91b960f697e47c37c
Juan Ferrero <juan.garcia.ferrero@cern.ch>
2016-09-21 11:47:17 +0200

    cut sequence redefined

--- include/CutsTTDM.h
+++ include/CutsTTDM.h
@@ -6,33 +6,22 @@
 
 enum {
   TTDM_00_Has2Leptons,
-  TTDM_01_Routin,
-  TTDM_presel_noMET,
-  TTDM_nobtag,
-  TTDM_03_Preselection,
-  TTDM_03_AN16105,
-  TTDM_04_tt,
-  TTDM_05_tt,
-  TTDM_06_tt,
-  TTDM_Zjets,
-  TTDM_WW0jet, 
-  TTDM_WW1jet,
+  TTDM_01_NewPresel,
+  TTDM_02_MET50,
+  //TTDM_03_AN16105,
+  //TTDM_04_tt,
+  //TTDM_05_tt,
+  //TTDM_06_tt,
+  //TTDM_Zjets,
+  //TTDM_WW0jet, 
+  //TTDM_WW1jet,
   ncut  // This line should be always last
 };
 
 const TString scut[ncut] = {
   "TTDM/00_Has2Leptons",
-  "TTDM/01_Routin",
-  "TTDM/presel_noMET",
-  "TTDM/nobtag",
-  "TTDM/03_Preselection",
-  "TTDM/03_AN16105",
-  "TTDM/04_tt",
-  "TTDM/05_tt",
-  "TTDM/06_tt",
-  "TTDM/Zjets",
-  "TTDM/WW0jet",
-  "TTDM/WW1jet"
+  "TTDM/01_NewPresel",
+  "TTDM/02_MET50"
 };
 
 #endif
