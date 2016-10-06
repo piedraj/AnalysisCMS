3002c8e61318c479962ddfc320726b6c20759a93
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-10-06 12:45:07 +0200

    Minor cleanup

--- submit-jobs.sh
+++ submit-jobs.sh
@@ -2,11 +2,36 @@
 
 if [ $# -lt 2 ]; then
     echo "  "
-    echo "  ./submit-jobs.sh <samples-file> nominal"
+    echo "  No skim"
+    echo "  "
+    echo "  ./submit-jobs.sh  samples/80x/l2loose/samples_data_l2loose_fakeW.txt  nominal"
+    echo "  ./submit-jobs.sh  samples/80x/l2tight/samples_data_l2tight.txt        nominal"
+    echo "  ./submit-jobs.sh  samples/80x/l2tight/samples_mc_l2tight.txt          nominal"
+    echo "  "
+    echo "  "
+    echo "  "
+    echo "  wwSel skim"
+    echo "  "
+    echo "  ./submit-jobs.sh  samples/80x/wwSel/samples_data_l2loose_fakeW.txt  nominal"
+    echo "  ./submit-jobs.sh  samples/80x/wwSel/samples_data_l2tight.txt        nominal"
+    echo "  ./submit-jobs.sh  samples/80x/wwSel/samples_mc_l2tight.txt          nominal"
+    echo "  ./submit-jobs.sh  samples/80x/wwSel/samples_monoh_l2tight.txt       nominal"
+    echo "  "
+    echo "  "
+    echo "  "
+    echo "  topSel skim"
+    echo "  "
+    echo "  ./submit-jobs.sh  samples/80x/topSel/samples_data_l2loose_fakeW.txt  nominal"
+    echo "  ./submit-jobs.sh  samples/80x/topSel/samples_data_l2tight.txt        nominal"
+    echo "  ./submit-jobs.sh  samples/80x/topSel/samples_mc_l2tight.txt          nominal"
+    echo "  ./submit-jobs.sh  samples/80x/topSel/samples_ttdm_l2tight.txt        nominal"
+    echo "  ./submit-jobs.sh  samples/80x/topSel/samples_stop_l2tight.txt        nominal"
     echo "  "
     exit -1
 fi
 
+
+export WORKDIRECTORY=$PWD
 export SAMPLES=$1
 export SYSTEMATIC=$2
 
@@ -25,4 +50,23 @@ export NJOBS=`cat $SAMPLES | grep latino | grep -v "#" | wc -l`
 echo "  And... submitting" $NJOBS "jobs"
 echo "  "
 mkdir -p jobs
-qsub -t 1-$NJOBS -v SAMPLES -v SYSTEMATIC settings.sge
+
+
+# lxplus
+#
+#    -q 8nm (8 minute)
+#    -q 1nh (1 hour)
+#    -q 8nh (8 hour)
+#    -q 1nd (1 day)
+#    -q 2nd (2 day)
+#    -q 1nw (1 week)
+#    -q 2nw (2 week)
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cd jobs
+bsub -q 8nh -J "settings[1-$NJOBS]" -o $WORKDIRECTORY/jobs $WORKDIRECTORY/settings.lsf WORKDIRECTORY SAMPLES SYSTEMATIC
+
+
+# gridui
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#qsub -t 1-$NJOBS -v SAMPLES -v SYSTEMATIC settings.sge
