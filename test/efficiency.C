69a28f711dd2a5fb586273b3bfa070eceede7e83
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-10-07 16:34:53 +0200

    Minor changes

--- test/efficiency.C
+++ test/efficiency.C
@@ -2,17 +2,27 @@
 //
 // Get the binomial errors for efficiencies
 //
+// root -l efficiency.C
+//
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const int nbins = 1;
+const int nbins = 7;
+
+float n_num[] = {  9847, 12193,  12774,  9940,  7060,  5540,   4080};
+float n_den[] = {100000, 99999, 100000, 97999, 99400, 98800, 100000};
 
 
 void efficiency()
 {
+  gInterpreter->ExecuteMacro("PaperStyle.C");
+
   TH1D* h_num = new TH1D("h_num", "h_num", nbins, 0, nbins);
   TH1D* h_den = new TH1D("h_den", "h_den", nbins, 0, nbins);
 
-  h_num->SetBinContent(1,  14000.);
-  h_den->SetBinContent(1, 100000.);
+  for (int i=0; i<nbins; i++)
+    {
+      h_num->SetBinContent(i+1, n_num[i]);
+      h_den->SetBinContent(i+1, n_den[i]);
+    }
 
   TGraphAsymmErrors* graph = new TGraphAsymmErrors();
 
