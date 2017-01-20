09f5f07a247d5d781a959d53e584203af282405a
Juan Ferrero <juan.garcia.ferrero@cern.ch>
2017-01-20 11:40:53 +0100

    merge renewed accordingly

--- test/merge.sh
+++ test/merge.sh
@@ -21,7 +21,7 @@ hadd -f -k 05_ST.root        ST_tW_antitop.root ST_tW_top.root
 hadd -f -k 06_WW.root        WWTo2L2Nu.root GluGluWWTo2L2Nu_MCFM.root
 hadd -f -k 07_ZJets.root     DYJetsToLL_M-10to50.root DYJetsToLL_M-50_000*.root
 hadd -f -k 08_WJets.root     WJetsToLNu.root
-hadd -f -k 09_TTV.root       TTWJetsToLNu.root TTZjets.root
+hadd -f -k 09_TTV.root       TTWJetsToLNu.root TTZToQQ.root TTZToLLNuNu_M-10.root
 hadd -f -k 10_HWW.root       GluGluHToWWTo2L2NuAMCNLO_M125.root VBFHToWWTo2L2Nu_M125.root GluGluHToTauTau_M125.root VBFHToTauTau_M125.root HWminusJ_HToWW_M125.root HWplusJ_HToWW_M125.root
 hadd -f -k 11_Wg.root        Wg_MADGRAPHMLM.root
 hadd -f -k 12_Zg.root        Zg.root
