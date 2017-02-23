a626527d7978df79ca94d31791c3a67f2cdaabd4
Barbara <chazinb@ifca.unican.es>
2017-02-23 12:44:51 +0100

    update 23 Feb 2017

--- include/Constants.h
+++ include/Constants.h
@@ -1,13 +1,13 @@
 #if !defined(MYLIB_CONSTANTS_H)
 #define MYLIB_CONSTANTS_H 1
 
-#include "CutsControl.h"
+//#include "CutsControl.h"
 //#include "CutsDY.h"
 //#include "CutsFR.h"
 //#include "CutsMonoH.h"
 //#include "CutsPR.h"
 //#include "CutsShape.h"
-//#include "CutsStop.h"
+#include "CutsStop.h"
 //#include "CutsTop.h"
 //#include "CutsTTDM.h"
 //#include "CutsWW.h"
@@ -21,7 +21,9 @@
 
 const double lumi_fb_2016       = 12.9;    // 2016B + 2016C + 2016D
 const double lumi_fb_Run2016B   =  5.892;  // From run 272007 to run 275376
-const double lumi_fb_2016_susy  =  4.394;  // From run 278820 to run 279931
+//const double lumi_fb_2016_susy  =  4.394;  // From run 278820 to run 279931
+const double lumi_blind_susy    =  10; 
+const double lumi_runBCDEFGH    =  37;  
 const double lumi_fb_2015       =  2.318;  // 2015C + 2015D
 const double lumi_error_percent =  6.2;
 
