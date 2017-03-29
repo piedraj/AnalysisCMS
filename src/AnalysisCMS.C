a9c4e5d9f45ee64a0fe0848410ff7dcf8a5fa8ad
Jonatan Piedra <jonatan.piedra@gmail.com>
2017-03-29 17:04:18 +0200

    Removed old l2tight samples.txt

--- src/AnalysisCMS.C
+++ src/AnalysisCMS.C
@@ -385,16 +385,16 @@ void AnalysisCMS::Setup(TString analysis,
   _isminitree   = (_filename.Contains("minitrees")) ? true : false;
   _isdatadriven = (_filename.Contains("fakeW")) ? "fakeW_" : "";
 
-  _dataperiod = "";
+  _dataperiod = "";  // TO BE REMOVED
 
-  if (_filename.Contains("21Jun2016_v2_Run2016B")) _dataperiod = "_21Jun2016";
-  if (_filename.Contains("05Jul2016_Run2016B"))    _dataperiod = "_05Jul2016";
-  if (_filename.Contains("08Jul2016_Run2016B"))    _dataperiod = "_08Jul2016";
-  if (_filename.Contains("08Jul2016_Run2016C"))    _dataperiod = "_08Jul2016";
-  if (_filename.Contains("11Jul2016_Run2016C"))    _dataperiod = "_11Jul2016";
-  if (_filename.Contains("15Jul2016_Run2016C"))    _dataperiod = "_15Jul2016";
-  if (_filename.Contains("15Jul2016_Run2016D"))    _dataperiod = "_15Jul2016";
-  if (_filename.Contains("26Jul2016_Run2016D"))    _dataperiod = "_26Jul2016";
+  //  if (_filename.Contains("21Jun2016_v2_Run2016B")) _dataperiod = "_21Jun2016";  // TO BE REMOVED
+  //  if (_filename.Contains("05Jul2016_Run2016B"))    _dataperiod = "_05Jul2016";  // TO BE REMOVED
+  //  if (_filename.Contains("08Jul2016_Run2016B"))    _dataperiod = "_08Jul2016";  // TO BE REMOVED
+  //  if (_filename.Contains("08Jul2016_Run2016C"))    _dataperiod = "_08Jul2016";  // TO BE REMOVED
+  //  if (_filename.Contains("11Jul2016_Run2016C"))    _dataperiod = "_11Jul2016";  // TO BE REMOVED
+  //  if (_filename.Contains("15Jul2016_Run2016C"))    _dataperiod = "_15Jul2016";  // TO BE REMOVED
+  //  if (_filename.Contains("15Jul2016_Run2016D"))    _dataperiod = "_15Jul2016";  // TO BE REMOVED
+  //  if (_filename.Contains("26Jul2016_Run2016D"))    _dataperiod = "_26Jul2016";  // TO BE REMOVED
 
   TString tok;
 
