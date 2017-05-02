e9a465b7d965afdfa8f77ced44cb2f5b69de30f3
scodella <luca.scodellaro@cern.ch>
2017-05-02 17:26:35 +0200

    protection for new isFRomISR variable

--- src/AnalysisCMS.C
+++ src/AnalysisCMS.C
@@ -830,7 +830,8 @@ void AnalysisCMS::GetJets(float jet_eta_max, float jet_pt_min)
 
     if (pt < 0.) continue;
 
-    if (pt>30. && fabs(eta)<2.4 && std_vector_jet_isFromISR->at(i)==1) _nisrjet++;
+    if (_isfastsim)
+      if (pt>30. && fabs(eta)<2.4 && std_vector_jet_isFromISR->at(i)==1) _nisrjet++;
 
     if (jet_eta_max > 0 && fabs(eta) > jet_eta_max) continue;
 
