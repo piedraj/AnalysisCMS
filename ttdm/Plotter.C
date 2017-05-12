1649029c9d17f8d54317746f8d9a8b0f096fa0d4
Cedric Prieels <prieelscedric@gmail.com>
2017-05-12 10:26:42 +0200

    Getting ready for synch with piedraj

--- ttdm/Plotter.C
+++ ttdm/Plotter.C
@@ -2,7 +2,7 @@
 // root -l -b -q Plotter.C 
 #include "../test/HistogramReader.C"
 
-const TString inputdir  = "histos/";
+const TString inputdir  = "histos";
 const TString outputdir = "figures/";
 //const TString outputdir = "/afs/cern.ch/user/j/jgarciaf/www/figures/Analysis_170321/";
 
@@ -66,14 +66,14 @@ void Plotter(TString option = "hist"){
 
   // Add processes
   //----------------------------------------------------------------------------
-  /*//plotter.AddProcess("14_HZ",        "HZ",         color_HZ);
-  //plotter.AddProcess("10_HWW",       "HWW",        color_HWW);
+  plotter.AddProcess("14_HZ",        "HZ",         color_HZ);
+  plotter.AddProcess("10_HWW",       "HWW",        color_HWW);
   plotter.AddProcess("06_WW",        "WW",         color_WW);
   plotter.AddProcess("02_WZTo3LNu",  "WZ",         color_WZTo3LNu);
   plotter.AddProcess("03_VZ",        "VZ",         color_VZ);
   plotter.AddProcess("13_VVV",       "VVV",        color_VVV);
   plotter.AddProcess("11_Wg",        "W#gamma",    color_Wg);
-  //plotter.AddProcess("15_WgStar",    "W#gamma*",   color_WgStar);
+  plotter.AddProcess("15_WgStar",    "W#gamma*",   color_WgStar);
   plotter.AddProcess("07_ZJets",     "Z+jets",     color_ZJets);
   plotter.AddProcess("09_TTV", "ttV",        color_TTV);
   plotter.AddProcess("04_TTTo2L2Nu", "tt",         color_TTTo2L2Nu);
@@ -83,13 +83,13 @@ void Plotter(TString option = "hist"){
 
   // Add signals
   //----------------------------------------------------------------------------
-  plotter.AddSignal("ttDM0001scalar00010", "m_{#chi}1 m_{S}10 x10",  color_Signal, roc_signal, 10);
+  plotter.AddSignal("ttDM0001scalar00010", "m_{#chi}1 m_{S}10",  color_Signal, roc_signal, 0.11);
   //plotter.AddSignal("ttDM0001scalar00020", "m_{#chi}1 m_{S}20",  color_Signal+2, roc_signal);
   //plotter.AddSignal("ttDM0001scalar00050", "m_{#chi}1 m_{S}50",  color_Signal+4, roc_signal);
-  //plotter.AddSignal("ttDM0001scalar00100", "m_{#chi}1 m_{S}100", color_Signal+2, roc_signal);
+  plotter.AddSignal("ttDM0001scalar00100", "m_{#chi}1 m_{S}100", color_Signal+2, roc_signal, 0.11);
   //plotter.AddSignal("ttDM0001scalar00200", "m_{#chi}1 m_{S}200", color_Signal, roc_signal);
   //plotter.AddSignal("ttDM0001scalar00300", "m_{#chi}1 m_{S}300", color_Signal, roc_signal);
-  //plotter.AddSignal("ttDM0001scalar00500", "m_{#chi}1 m_{S}500 x10^{3}", color_Signal+4, roc_signal, 1000);
+  plotter.AddSignal("ttDM0001scalar00500", "m_{#chi}1 m_{S}500", color_Signal+4, roc_signal, 0.11);
 
 
   // Add systematics
@@ -109,7 +109,7 @@ void Plotter(TString option = "hist"){
 
   //            hname              xtitle                     ngroup precision units  setlogy moveoverflow xmin xmax ymin ymax
   // -------------------------------------------------------------------------------------------------------------------------
-  //plotter.Draw( "newdarkpt"         , "(reconstructed) mediator p_{T}",      5, 0, "GeV",  scale, false, -100, 800 );
+  plotter.Draw( "darkpt"         , "(reconstructed) mediator p_{T}",      5, 0, "GeV",  scale, false, 0, 800 );
   //plotter.Draw( "dphijet1met"    , "#Delta#phi(jet1,E_{T}^{miss})",      5, 2, "rad",  scale, false);
   //plotter.Draw( "dphijet2met"    , "#Delta#phi(jet2,E_{T}^{miss})",      5, 2, "rad",  scale, false);
   //plotter.Draw( "dphijj"         , "#Delta#phi(jet1,jet2)",              5, 2, "rad",  scale, false);
@@ -134,32 +134,32 @@ void Plotter(TString option = "hist"){
   //plotter.Draw( "jet2phi"        , "trailing jet #phi",                  5, 2, "rad",  scale, false);
   //plotter.Draw( "jet2pt"         , "trailing jet p_{T}",                 5, 0, "GeV",  scale, true, 0,  400);
   plotter.Draw( "lep1eta"        , "leading lepton #eta",               -1, 1, "NULL", scale);
-  //plotter.Draw( "lep1phi"        , "leading lepton #phi",                5, 2, "rad",  scale);
+  plotter.Draw( "lep1phi"        , "leading lepton #phi",                5, 2, "rad",  scale);
   plotter.Draw( "lep1pt"         , "leading lepton p_{T}",               5, 0, "GeV",  scale, true, 0,  150, 1);
-  //plotter.Draw( "lep2eta"        , "trailing lepton #eta",              -1, 1, "NULL", scale);
-  //plotter.Draw( "lep2phi"        , "trailing lepton #phi",               5, 2, "rad",  scale);
+  plotter.Draw( "lep2eta"        , "trailing lepton #eta",              -1, 1, "NULL", scale);
+  plotter.Draw( "lep2phi"        , "trailing lepton #phi",               5, 2, "rad",  scale);
   plotter.Draw( "lep2pt"         , "trailing lepton p_{T}",              5, 0, "GeV",  scale, true, 0,  150, 1);
-  //plotter.Draw( "m2l"            , "m_{" + sll + "}",                   10, 0, "GeV",  scale, true, 0,  300, 1);
+  plotter.Draw( "m2l"            , "m_{" + sll + "}",                   10, 0, "GeV",  scale, true, 0,  300, 1);
   plotter.Draw( "metPfType1"     , sm,                                  10, 0, "GeV",  scale, true, 0,  200, 1);
   //plotter.Draw( "mt2lblb"        , "M_{T2}(" + sl + "b" + sl + "b)",    10, 0, "GeV",  scale, false, 0, 600);
-  //plotter.Draw( "mt2ll"          , "M_{T2}(" + sll + ")",               10, 0, "GeV",  scale, false, 0, 200, 1); 
+  plotter.Draw( "mt2ll"          , "M_{T2}(" + sll + ")",               10, 0, "GeV",  scale, false, 0, 200, 1); 
   //plotter.Draw( "mtw1"           , "m_{T}^{W,1}",                       10, 0, "GeV",  scale, true, 0,  400);
   //plotter.Draw( "mtw2"           , "m_{T}^{W,2}",                       10, 0, "GeV",  scale, true, 0,  400);
-  //plotter.Draw( "nbjet30csvv2m"  , "number of 30 GeV csvv2m b-jets",    -1, 0, "NULL", scale, true, 0,  6, 1);
-  //plotter.Draw( "njet"           , "number of 30 GeV jets",             -1, 0, "NULL", scale, true, 0,  10, 1);
+  plotter.Draw( "nbjet30csvv2m"  , "number of 30 GeV csvv2m b-jets",    -1, 0, "NULL", scale, true, 0,  6, 1);
+  plotter.Draw( "njet"           , "number of 30 GeV jets",             -1, 0, "NULL", scale, true, 0,  10, 1);
   plotter.Draw( "nvtx"           , "number of vertices",                -1, 0, "NULL", linY,  true, 0,   30);
   //plotter.Draw( "topRecoW"       , "top reco weight",                  -1, 4, "NULL", scale, true, 0,  0.01);
-  plotter.Draw( "ANN_0704sigmoid_mt2ll80_ttDM0001scalar00010", "ANN sigmoid output",  10, 2, "NULL", linY, true, -0.1, 1.1);
-  plotter.Draw( "ANN_0704tanh_mt2ll80_ttDM0001scalar00010", "ANN tanh output",  10, 2, "NULL", linY, true, -0.1, 1.1);
+  //plotter.Draw( "ANN_0704sigmoid_mt2ll80_ttDM0001scalar00010", "ANN sigmoid output",  2, 2, "NULL", linY, true, -0.1, 1.1);
+  //plotter.Draw( "ANN_0704tanh_mt2ll80_ttDM0001scalar00010", "ANN tanh output",  2, 2, "NULL", linY, true, -0.1, 1.1);
   //plotter.Draw( "ANN_170222_mt2ll90_ttDM0001scalar00100", "ANN output",  10, 2, "NULL", scale, true, -0.1, 1.0);
   //plotter.Draw( "ANN_170222_mt2ll100_ttDM0001scalar00100", "ANN output", 10, 2, "NULL", scale, true, -0.1, 1.0);
   //plotter.Draw( "ANN_alpha005_mt2ll80_ttDM0001scalar00500", "ANN output", 10, 2, "NULL", scale, true, 0, 1.0);
   //plotter.Draw( "ANN_alpha01_mt2ll80_ttDM0001scalar00500", "ANN output", 10, 2, "NULL", scale, true, 0, 1.0);
   //plotter.Draw( "ANN_alpha05_mt2ll80_ttDM0001scalar00500", "ANN output", 10, 2, "NULL", scale, true, 0, 1.0);
 
-  plotter.Draw( "scale"           , "- u_{||} / q_{T}",                -1, 0, "NULL", scale,  true, -3, 5, 1, 1e8 );
-  plotter.Draw( "uPara"           , "u_{||} + q_{T}",                  -1, 0, "GeV", scale,  true, -200, 200, 1, 1e8 );
-  plotter.Draw( "uPerp"           , "u_{#perp}",                       -1, 0, "GeV", scale,  true, -200, 200, 1, 1e8 );
+  //plotter.Draw( "scale"           , "- u_{||} / q_{T}",                -1, 0, "NULL", scale,  true, -3, 5, 1, 1e8 );
+  //plotter.Draw( "uPara"           , "u_{||} + q_{T}",                  -1, 0, "GeV", scale,  true, -200, 200, 1, 1e8 );
+  //plotter.Draw( "uPerp"           , "u_{#perp}",                       -1, 0, "GeV", scale,  true, -200, 200, 1, 1e8 );
 
   //           hname     xtitle                      npoints  units xmin xmax
   //  ---------------------------------------------------------------------------
