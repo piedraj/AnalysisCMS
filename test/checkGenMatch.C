e55bc98124c74f26f59c012b461d0645e2fa1fec
Jonatan Piedra <jonatan.piedra@gmail.com>
2017-06-02 14:57:20 +0200

    Fractions of mothers added

--- test/checkGenMatch.C
+++ test/checkGenMatch.C
@@ -25,8 +25,10 @@ const double MUON_MASS     = 0.106;     // [GeV]
 //    root -l -b -q checkGenMatch.C+
 //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void checkGenMatch(TString filename = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/Full2016/Feb2017_summer16/MCl2looseCut__hadd__bSFL2pTEffCut__l2tight/latino_DYJetsToLL_M-50__part0.root")
+void checkGenMatch(TString filename = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/Full2016/Feb2017_summer16/MCl2looseCut__hadd__bSFL2pTEffCut__l2tight/latino_WZTo2L2Q__part0.root")
 {
+  printf("\n Reading %s\n", filename.Data());
+
   TFile* file = TFile::Open(filename);
 
   TTree* tree = (TTree*)file->Get("latino");
@@ -64,19 +66,54 @@ void checkGenMatch(TString filename = "/eos/cms/store/group/phys_higgs/cmshww/am
   tree->SetBranchAddress("std_vector_leptonGen_pt",                            &std_vector_leptonGen_pt);
   tree->SetBranchAddress("std_vector_leptonGen_status",                        &std_vector_leptonGen_status);
 
-  int counter_genmatched       = 0;  // Events currently matched in the latino framework
-  int counter_genpromptmatched = 0;  // Events with more strict matching
+  int counter_genmatched = 0;
+
+  int counter1_nonprompt          = 0;
+  int counter1_mother_1_d         = 0;
+  int counter1_mother_2_u         = 0;
+  int counter1_mother_3_s         = 0;
+  int counter1_mother_4_c         = 0;
+  int counter1_mother_5_b         = 0;
+  int counter1_mother_6_t         = 0;
+  int counter1_mother_11_electron = 0;
+  int counter1_mother_13_muon     = 0;
+  int counter1_mother_15_tau      = 0;
+  int counter1_mother_21_gluon    = 0;
+  int counter1_mother_22_photon   = 0;
+  int counter1_mother_23_Z        = 0;
+  int counter1_mother_24_W        = 0;
+  int counter1_mother_2212_proton = 0;
+  int counter1_mother_other       = 0;
+
+  int counter2_nonprompt          = 0;
+  int counter2_mother_1_d         = 0;
+  int counter2_mother_2_u         = 0;
+  int counter2_mother_3_s         = 0;
+  int counter2_mother_4_c         = 0;
+  int counter2_mother_5_b         = 0;
+  int counter2_mother_6_t         = 0;
+  int counter2_mother_11_electron = 0;
+  int counter2_mother_13_muon     = 0;
+  int counter2_mother_15_tau      = 0;
+  int counter2_mother_21_gluon    = 0;
+  int counter2_mother_22_photon   = 0;
+  int counter2_mother_23_Z        = 0;
+  int counter2_mother_24_W        = 0;
+  int counter2_mother_2212_proton = 0;
+  int counter2_mother_other       = 0;
 
 
   // Loop over events
   //----------------------------------------------------------------------------
   Long64_t nentries = tree->GetEntries();
 
+  if (nentries > 1e5) nentries = 1e5;
+
   for (Long64_t jentry=0; jentry<nentries;jentry++) {
 
     tree->GetEntry(jentry);
 
-    if (std_vector_lepton_pt->at(0) < 10) continue;
+    if (std_vector_lepton_pt->at(0) < 25) continue;
     if (std_vector_lepton_pt->at(1) < 10) continue;
 
     if (abs(std_vector_lepton_flavour->at(0)) != 11 && abs(std_vector_lepton_flavour->at(0)) != 13) continue;
@@ -92,8 +129,6 @@ void checkGenMatch(TString filename = "/eos/cms/store/group/phys_higgs/cmshww/am
     //--------------------------------------------------------------------------
     UInt_t firstLeptonGen  = 999;
     UInt_t secondLeptonGen = 999;
-
-    Bool_t event_genpromptmatched = false;
 
     for (UInt_t i=0; i<2; i++) {
 
@@ -131,41 +166,96 @@ void checkGenMatch(TString filename = "/eos/cms/store/group/phys_higgs/cmshww/am
 					std_vector_leptonGen_mass);
 
 
-	// Do the matching
+	// Get the mother PID
 	//----------------------------------------------------------------------
 	if (lepton_tlorentz.DeltaR(leptonGen_tlorentz) < deltaRMin) {
 
 	  if (i == 0) firstLeptonGen = j;
 
-	  if (i == 1 && firstLeptonGen > 10) continue;
-
 	  if (i == 1 && j == firstLeptonGen) continue;
 
 	  if (i == 1) secondLeptonGen = j;
 
-	  if (i == 1 && secondLeptonGen > 10) continue;
-
 	  deltaRMin = lepton_tlorentz.DeltaR(leptonGen_tlorentz);
-
-	  if (abs(std_vector_leptonGen_MotherPID->at(j)) == 15 ||  // tau
-	      abs(std_vector_leptonGen_MotherPID->at(j)) == 23 ||  // Z
-	      abs(std_vector_leptonGen_MotherPID->at(j)) == 24) {  // W
-
-	    event_genpromptmatched = true;
-	  }
 	}
       }
     }
 
-    if (event_genpromptmatched) counter_genpromptmatched++;
+
+    // Fill the counters
+    //--------------------------------------------------------------------------
+    if (firstLeptonGen  == 999) ++counter1_nonprompt;
+    if (secondLeptonGen == 999) ++counter2_nonprompt;
+
+    if (firstLeptonGen < 999 && secondLeptonGen < 999) {
+
+      int lep1mpid = abs(std_vector_leptonGen_MotherPID->at(firstLeptonGen));
+      int lep2mpid = abs(std_vector_leptonGen_MotherPID->at(secondLeptonGen));
+
+      if      (lep1mpid ==    1) ++counter1_mother_1_d;
+      else if (lep1mpid ==    2) ++counter1_mother_2_u;
+      else if (lep1mpid ==    3) ++counter1_mother_3_s;
+      else if (lep1mpid ==    4) ++counter1_mother_4_c;
+      else if (lep1mpid ==    5) ++counter1_mother_5_b;
+      else if (lep1mpid ==    6) ++counter1_mother_6_t;
+      else if (lep1mpid ==   11) ++counter1_mother_11_electron;
+      else if (lep1mpid ==   13) ++counter1_mother_13_muon;
+      else if (lep1mpid ==   15) ++counter1_mother_15_tau;
+      else if (lep1mpid ==   21) ++counter1_mother_21_gluon;
+      else if (lep1mpid ==   22) ++counter1_mother_22_photon;
+      else if (lep1mpid ==   23) ++counter1_mother_23_Z;
+      else if (lep1mpid ==   24) ++counter1_mother_24_W;
+      else if (lep1mpid == 2212) ++counter1_mother_2212_proton;
+      else {
+	++counter1_mother_other;
+	printf(" lep1mpid: %d\n", lep1mpid);
+      }
+
+      if      (lep2mpid ==    1) ++counter2_mother_1_d;
+      else if (lep2mpid ==    2) ++counter2_mother_2_u;
+      else if (lep2mpid ==    3) ++counter2_mother_3_s;
+      else if (lep2mpid ==    4) ++counter2_mother_4_c;
+      else if (lep2mpid ==    5) ++counter2_mother_5_b;
+      else if (lep2mpid ==    6) ++counter2_mother_6_t;
+      else if (lep2mpid ==   11) ++counter2_mother_11_electron;
+      else if (lep2mpid ==   13) ++counter2_mother_13_muon;
+      else if (lep2mpid ==   15) ++counter2_mother_15_tau;
+      else if (lep2mpid ==   21) ++counter2_mother_21_gluon;
+      else if (lep2mpid ==   22) ++counter2_mother_22_photon;
+      else if (lep2mpid ==   23) ++counter2_mother_23_Z;
+      else if (lep2mpid ==   24) ++counter2_mother_24_W;
+      else if (lep2mpid == 2212) ++counter2_mother_2212_proton;
+      else {
+	++counter2_mother_other;
+	printf(" lep2mpid: %d\n", lep2mpid);
+      }
+    }
   }
 
 
   // Print the results
   //----------------------------------------------------------------------------
+  float factor = 1e2 / counter_genmatched;
+
   printf("\n");
-  printf(" number of events genmatched       = %d\n", counter_genmatched);
-  printf(" number of events genpromptmatched = %d\n", counter_genpromptmatched);
-  printf(" fraction of duplicates            = %.2f%% for %s\n", 1e2 - 1e2*counter_genpromptmatched/counter_genmatched, filename.Data());
+  printf(" number of events genmatched: %d for %s\n\n", counter_genmatched, filename.Data());
+  printf(" mother PID |    lep1 |    lep2\n");
+  printf("------------+---------+---------\n");
+  printf(" non-prompt | %6.2f%% | %6.2f%%\n", factor * counter1_nonprompt,          factor * counter2_nonprompt);
+  printf(" d          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_1_d,         factor * counter2_mother_1_d);
+  printf(" u          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_2_u,         factor * counter2_mother_2_u);
+  printf(" s          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_3_s,         factor * counter2_mother_3_s);
+  printf(" c          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_4_c,         factor * counter2_mother_4_c);
+  printf(" b          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_5_b,         factor * counter2_mother_5_b);
+  printf(" t          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_6_t,         factor * counter2_mother_6_t);
+  printf(" electron   | %6.2f%% | %6.2f%%\n", factor * counter1_mother_11_electron, factor * counter2_mother_11_electron);
+  printf(" muon       | %6.2f%% | %6.2f%%\n", factor * counter1_mother_13_muon,     factor * counter2_mother_13_muon);
+  printf(" tau        | %6.2f%% | %6.2f%%\n", factor * counter1_mother_15_tau,      factor * counter2_mother_15_tau);
+  printf(" gluon      | %6.2f%% | %6.2f%%\n", factor * counter1_mother_21_gluon,    factor * counter2_mother_21_gluon);
+  printf(" photon     | %6.2f%% | %6.2f%%\n", factor * counter1_mother_22_photon,   factor * counter2_mother_22_photon);
+  printf(" Z          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_23_Z,        factor * counter2_mother_23_Z);
+  printf(" W          | %6.2f%% | %6.2f%%\n", factor * counter1_mother_24_W,        factor * counter2_mother_24_W);
+  printf(" proton     | %6.2f%% | %6.2f%%\n", factor * counter1_mother_2212_proton, factor * counter2_mother_2212_proton);
+  printf(" other      | %6.2f%% | %6.2f%%\n", factor * counter1_mother_other,       factor * counter2_mother_other);
   printf("\n");
 }
