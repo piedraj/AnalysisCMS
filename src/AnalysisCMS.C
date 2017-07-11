ca796604d654826671fe2f3767109fb5620ca539
Jonatan Piedra <jonatan.piedra@gmail.com>
2017-07-11 11:12:16 +0200

    Minor changes

--- src/AnalysisCMS.C
+++ src/AnalysisCMS.C
@@ -429,7 +429,8 @@ void AnalysisCMS::ApplyWeights()
   GetSampleWeight();
 
   // https://github.com/latinos/LatinoAnalysis/blob/master/Gardener/python/variables/genMatchVar.py
-  _event_weight_genmatched = std_vector_lepton_promptgenmatched->at(0) * std_vector_lepton_promptgenmatched->at(1);
+  //  _event_weight_genmatched = std_vector_lepton_promptgenmatched->at(0) * std_vector_lepton_promptgenmatched->at(1);  // TO BE ADDED BY XAVIER
+  _event_weight_genmatched = std_vector_lepton_genmatched->at(0) * std_vector_lepton_genmatched->at(1);                  // DEPRECATED
 
   if (!_analysis.EqualTo("TTDM") && !_analysis.EqualTo("Stop")) _event_weight *= _event_weight_genmatched;
 
