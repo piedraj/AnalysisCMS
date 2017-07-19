bca817b5b5bcd2546ad625b887d3dd1b56dab4c2
Jonatan Piedra <jonatan.piedra@gmail.com>
2017-07-19 16:11:37 +0200

    Updated

--- samples/write-samples.sh
+++ samples/write-samples.sh
@@ -3,7 +3,7 @@
 if [ $# -lt 3 ]; then
     echo "  "
     echo "  ./write-samples.sh \\"
-    echo "  '/eos/cms/store/group/phys_higgs/cmshww/amassiro/Full2016_Apr17/Apr2017_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC__l2loose__hadd__l2tightOR__formulasMC__MCFix/' \\"
+    echo "  '/eos/cms/store/group/phys_higgs/cmshww/amassiro/Full2016_Apr17/Apr2017_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC__l2loose__hadd__l2tightOR__formulasMC/' \\"
     echo "  mc-names.txt \\"
     echo "  samples_mc_l2tightOR.txt"
     echo "  "
@@ -29,6 +29,8 @@ fi
 export SAMPLES_PATH=$1
 export SAMPLES_NAMES=$2
 export SAMPLES_FILES=$3
+
+rm -rf $SAMPLES_FILES
 
 for MYPATH in "${SAMPLES_PATH[@]}"; do
     awk -v dir=$MYPATH '{ print "ls",dir"latino_"$1 }' $SAMPLES_NAMES | bash > $SAMPLES_FILES
