ac785568dc2f8f15474b976d435e15c6c65f9f6f
Juan Ferrero <juan.garcia.ferrero@cern.ch>
2017-08-03 14:35:06 +0200

    more about limits

---
+++ ttdm/executeLimits/hadding-pseudo.sh
@@ -0,0 +1,7 @@
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00010_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH1*.root
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00020_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH2*.root
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00050_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH3*.root
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00100_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH4*.root
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00200_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH5*.root
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00300_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH6*.root
+hadd -f /afs/cern.ch/user/j/jgarciaf/www/txt-files/datacards/170803/ttDM0001pseudo00500_camille_combined_optimisation.root  higgsCombinettDM.Asymptotic.mH7*.root
