77e34d8cab3405cf5e073d0eff4ae218c7228ee2
Jonatan Piedra <jonatan.piedra@gmail.com>
2017-09-27 16:46:10 +0200

    Minor changes

--- test/lepton_idiso_syst.C
+++ test/lepton_idiso_syst.C
@@ -1,11 +1,18 @@
-const TString directory = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/Full2016_Apr17/Apr2017_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC__l2loose__hadd__l2tightOR__formulasMC/";
-
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
 // root -l lepton_idiso_syst.C
 //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Source code
+//
+// https://github.com/latinos/LatinoAnalysis/blob/master/Gardener/python/variables/multiIdisoScaleFactors.py#L556-L559
+// https://github.com/latinos/LatinoAnalysis/blob/master/Gardener/python/data/formulasToAdd_MC.py#L80-L90 
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const TString directory = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/Full2016_Apr17/Apr2017_summer16/lepSel__MCWeights__bSFLpTEffMulti__cleanTauMC__l2loose__hadd__l2tightOR__formulasMC/";
+
+
 void lepton_idiso_syst(TString filename = "latino_WWTo2L2Nu.root")
 {
   // Read
@@ -31,7 +38,7 @@ void lepton_idiso_syst(TString filename = "latino_WWTo2L2Nu.root")
   h_ele->SetFillColor(kAzure);
   h_ele->SetLineColor(kAzure);
 
-  latino->Draw("0.5*1e2*(LepSF2l__ele_cut_WP_Tight80X__Up-LepSF2l__ele_cut_WP_Tight80X__Do)/LepSF2l__ele_cut_WP_Tight80X__mu_cut_Tight80x>>h_ele", lepton_pt && electrons);
+  latino->Draw("0.5*1e2*(LepSF2l__ele_cut_WP_Tight80X__Up-LepSF2l__ele_cut_WP_Tight80X__Do)>>h_ele", lepton_pt && electrons);
 
   c_ele->SaveAs("electron_idiso_syst.png");
 
@@ -45,7 +52,7 @@ void lepton_idiso_syst(TString filename = "latino_WWTo2L2Nu.root")
   h_mu->SetFillColor(kAzure);
   h_mu->SetLineColor(kAzure);
 
-  latino->Draw("0.5*1e2*(LepSF2l__mu_cut_Tight80x__Up-LepSF2l__mu_cut_Tight80x__Do)/LepSF2l__ele_cut_WP_Tight80X__mu_cut_Tight80x>>h_mu", lepton_pt && muons);
+  latino->Draw("0.5*1e2*(LepSF2l__mu_cut_Tight80x__Up-LepSF2l__mu_cut_Tight80x__Do)>>h_mu", lepton_pt && muons);
 
   c_mu->SaveAs("muon_idiso_syst.png");
 
