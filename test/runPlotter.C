a6b262684591b47a02e300f59e7ee6c97e0d2df2
Jonatan Piedra <jonatan.piedra@gmail.com>
2017-10-19 13:06:10 +0200

    Minor changes

--- test/runPlotter.C
+++ test/runPlotter.C
@@ -104,7 +104,7 @@ void runPlotter(TString level,
     {
       plotter.AddPrefit("99_Prefit", "pre-fit", kBlue+1);
 
-      plotter.AddSignal("ttDM0001pseudo00100", "PS M_{#Phi}=100 GeV, M_{#chi}=1 GeV x1000", color_Signal, roc_background, 250);
+      plotter.AddSignal("ttDM0001pseudo00100", "PS M_{#Phi}=100 GeV, M_{#chi}=1 GeV x200", color_Signal, roc_background, 200);
     }
 
 
