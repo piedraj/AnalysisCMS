a610c6fffab83976f3496250a8db61fe3e1521dc
Jonatan Piedra <jonatan.piedra@gmail.com>
2018-07-03 15:47:01 +0200

    Added HLTsafe selection

--- src/AnalysisBase.C
+++ src/AnalysisBase.C
@@ -1,75 +1,110 @@
 #define AnalysisBase_cxx
 #include "../include/AnalysisBase.h"
-#include <TH2.h>
-#include <TStyle.h>
-#include <TCanvas.h>
+
 
 void AnalysisBase::Loop()
 {
   printf(" <<< Entering [AnalysisBase::Loop]\n");  // Debug
 
-//   In a ROOT session, you can do:
-//      root> .L AnalysisBase.C
-//      root> AnalysisBase t
-//      root> t.GetEntry(12); // Fill t data members with entry number 12
-//      root> t.Show();       // Show values of entry 12
-//      root> t.Show(16);     // Read and show values of entry 16
-//      root> t.Loop();       // Loop on all entries
-//
+  if (fChain == 0) return;
 
-//     This is the loop skeleton where:
-//    jentry is the global entry number in the chain
-//    ientry is the entry number in the current Tree
-//  Note that the argument to GetEntry must be:
-//    jentry for TChain::GetEntry
-//    ientry for TTree::GetEntry and TBranch::GetEntry
-//
-//       To read only selected branches, Insert statements like:
-// METHOD1:
-//    fChain->SetBranchStatus("*",0);  // disable all branches
-//    fChain->SetBranchStatus("branchname",1);  // activate branchname
-// METHOD2: replace line
-//    fChain->GetEntry(jentry);       //read all branches
-//by  b_branchname->GetEntry(ientry); //read only this branch
-   if (fChain == 0) return;
+  Long64_t nentries = fChain->GetEntriesFast();
 
-   Long64_t nentries = fChain->GetEntriesFast();
+  printf(" nentries = %lld\n", nentries);  // Please remove me when debug has finished
 
-   printf(" nentries = %lld\n", nentries);  // Debug
+  if (nentries > 200) nentries = 200;  // Please remove me when debug has finished
 
-   if (nentries > 200) nentries = 200;  // Debug. Please remove me when debug has finished
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
 
-   Long64_t nbytes = 0, nb = 0;
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
-      // if (Cut(ientry) < 0) continue;
+    Long64_t ientry = LoadTree(jentry);
+
+    if (ientry < 0) break;
+
+    fChain->GetEntry(jentry);
 
 
-      if (nLepton == nMuon)     continue;
-      if (nLepton == nElectron) continue;
+    printf("\n------------------------------------------------\n");
 
 
-      printf("\n------------------------------------------------\n");
+    //
+    // https://raw.githubusercontent.com/latinos/LatinoAnalysis/master/NanoGardener/python/data/LeptonSel_cfg.py
+    //    Study2017
+    //       FakeObjWP
+    //
 
 
-      // Lepton
-      //------------------------------------------------------------------------
-      for (UInt_t i=0; i<nLepton; i++)
-	printf(" Lepton i   = %d (nLepton   = %d);  pt = %6.2f;  eta = %5.2f;  eIdx = %2d;  mIdx = %2d\n",
-	       i, nLepton, Lepton_pt[i], Lepton_eta[i], Lepton_electronIdx[i], Lepton_muonIdx[i]);
+    // Lepton
+    //--------------------------------------------------------------------------
+    for (UInt_t i=0; i<nLepton; i++)
+      printf(" lep %2d;  pt = %6.2f;  eta = %5.2f;  eIdx = %2d;  mIdx = %2d\n",
+	     i,
+	     Lepton_pt[i],
+	     Lepton_eta[i],
+	     Lepton_electronIdx[i],
+	     Lepton_muonIdx[i]);
 
 
-      // Muon
-      //------------------------------------------------------------------------
-      for (UInt_t j=0; j<nMuon; j++)
-	printf(" Muon   j   = %d (nMuon     = %d);  pt = %6.2f;  eta = %5.2f\n", j, nMuon, Muon_pt[j], Muon_eta[j]);
+    Bool_t pass;
 
 
-      // Electron
-      //------------------------------------------------------------------------
-      for (UInt_t k=0; k<nElectron; k++)
-	printf(" Electron k = %d (nElectron = %d);  pt = %6.2f;  eta = %5.2f\n", k, nElectron, Electron_pt[k], Electron_eta[k]);
-   }
+    // Muon
+    //--------------------------------------------------------------------------
+    for (UInt_t j=0; j<nMuon; j++) {
+
+      pass = true;
+
+      pass &= (fabs(Muon_eta[j]) < 2.4);
+    //pass &= (Muon_mediumId[j] == 1);  // NOT USED
+      pass &= (Muon_pfRelIso04_all[j] < 0.4);
+
+      printf(" muo %2d;  pt = %6.2f;  eta = %5.2f;  pass = %2d;  mediumId = %d;  pfRelIso04 = %5.2f\n",
+	     j,
+	     Muon_pt[j],
+	     Muon_eta[j],
+	     pass,
+	     Muon_mediumId[j],
+	     Muon_pfRelIso04_all[j]);
+    }
+
+
+    // Electron
+    //--------------------------------------------------------------------------
+    for (UInt_t k=0; k<nElectron; k++) {
+
+      pass = true;
+
+    //pass &= (fabs(Electron_eta[k] < 2.5);  // NOT USED
+      pass &= (Electron_lostHits[k] < 2);
+      pass &= (Electron_convVeto[k] == 1);
+      pass &= (Electron_eInvMinusPInv[k] < 0.013);
+    //pass &= (Electron_pfRelIso03_all[k] < 0.06);  // NOT USED
+      pass &= (Electron_dr03TkSumPt[k] < 0.1);
+      pass &= (Electron_dr03HcalDepth1TowerSumEt[k]/Electron_pt[k] < 0.12);
+
+      Bool_t passBarrel = true;
+
+      passBarrel &= (fabs(Electron_eta[k]) <= 1.479);
+    //passBarrel &= (fabs(Electron_dxy[k]) < 0.05);  // NOT USED
+    //passBarrel &= (fabs(Electron_dz[k]) < 0.1);  // NOT USED
+      passBarrel &= (fabs(Electron_deltaEtaSC[k]) < 0.004);
+      passBarrel &= (Electron_sieie[k] < 0.011);
+      passBarrel &= (Electron_hoe[k] < 0.06);
+      passBarrel &= (Electron_dr03EcalRecHitSumEt[k]/Electron_pt[k] < 0.15);
+
+      Bool_t passEndcap = true;
+
+      passEndcap &= (fabs(Electron_eta[k]) > 1.479);
+    //passEndcap &= (fabs(Electron_dxy[k]) < 0.1);  // NOT USED
+    //passEndcap &= (fabs(Electron_dz[k]) < 0.2);  // NOT USED
+      passEndcap &= (Electron_sieie[k] < 0.03);
+      passEndcap &= (Electron_hoe[k] < 0.07);
+      passEndcap &= (Electron_dr03EcalRecHitSumEt[k]/Electron_pt[k] < 0.13);
+
+      printf(" ele %2d;  pt = %6.2f;  eta = %5.2f;  pass = %2d\n",
+	     k,
+	     Electron_pt[k],
+	     Electron_eta[k],
+	     pass);
+    }
+  }
 }
