e0d1cdea2d149f797fbcc7e7a0cd9fa907278869
Jonatan Piedra <jonatan.piedra@gmail.com>
2018-08-02 14:17:07 +0200

    Minor cleanup

--- test/runTestNano.C
+++ test/runTestNano.C
@@ -5,8 +5,7 @@
 // Last update: August 1st, 2018
 //
 //------------------------------------------------------------------------------
-
-TString path_mc = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Fall2017_nAOD_v1_Study2017/MCl1loose2017__baseW/";
+TString path_mc = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Fall2017_nAOD_v1_Study2017/MCl1loose2017__baseW__hadd/";
 
 TString path_data = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Run2017_nAOD_v1_Study2017/DATAl1loose2017__hadd/";
 
@@ -16,9 +15,10 @@ void runTestNano(TString filename = "NONE")
   if (filename.EqualTo("NONE"))
     {
       printf("\n Examples\n\n");
-      printf(" root -l -b -q \'runTestNano.C(\"nanoLatino_SingleMuon_Run2017E-31Mar2018-v1__part2.root\")\'\n");
-      printf(" root -l -b -q \'runTestNano.C(\"nanoLatino_SingleElectron_Run2017E-31Mar2018-v1__part2.root\")\'\n");
-      printf(" root -l -b -q \'runTestNano.C(\"nanoLatino_WWW__part0.root\")\'\n");
+      printf(" root -l -b -q \'runTestNano.C(\"SingleMuon_Run2017E-31Mar2018-v1__part2\")\'\n");
+      printf(" root -l -b -q \'runTestNano.C(\"SingleElectron_Run2017E-31Mar2018-v1__part2\")\'\n");
+      printf(" root -l -b -q \'runTestNano.C(\"WWW\")\'\n");
+      printf(" root -l -b -q \'runTestNano.C(\"GluGluHToWWTo2L2NuPowheg_M125_private\")\'\n");
       printf("\n");
       
       return;
@@ -28,7 +28,7 @@ void runTestNano(TString filename = "NONE")
 
   TChain* mychain = new TChain("Events", "Events");
 
-  mychain->Add(path + filename);
+  mychain->Add(path + "nanoLatino_" + filename + ".root");
 
   printf("\nExecuting mychain->MakeSelector(\"testNano\")...\n\n");
 
